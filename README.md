bc0c5c6244e1cc571e1ec17fdf62e2cfa2425dfe
ah-hui <673350601@qq.com>
2018-12-25 17:31:47 +0800

    [ADD] SBA监控加入登录验证；

--- README.md
+++ README.md
@@ -10,4 +10,8 @@
 # T01分支
 - 使用eureka负责发现，SBA server注册到eureka实现监控，不再依赖和使用SBA client
 - 此时其他服务只需要注册到eureka即可，不用依赖SBA client，当然你也可以依赖SBA client并使用它，完全不使用eureka（这就比较逗比了别忘了你玩的是cloud）实现监控
-
+- SBA服务端可以访问客户端的敏感端点，因此手册建议我们应该为它们添加安全配置
+- 安全配置只需在server做，但是客户端要对应配置好用户名密码，否则客户端无法注册到SBA server（eureka发现不受影响）
+- 加入密码后发现大家多了个配置metadataMap。Eureka 中的 metadataMap 是专门用来存放一些自定义的数据，当注册中心或者其他服务需要此服务的某些配置时可以在 metadataMap 里取。实际上，每个 instance 都有各自的 metadataMap，map 中存放着需要用到的属性。
+- 另外，你会发现，eureka不带metadataMap（用户名密码）也能注册上去，脸白的一匹
+- 还能玩的更花，通知：当已注册到SBA server的client发生某些事件的时候，可以收到通知，比如使用email实现
---
+++ admin-server/src/main/java/com/suneee/server/config/SecureConfig.java
@@ -0,0 +1,41 @@
+package com.suneee.server.config;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+/**
+ * 功能描述：.
+ *
+ * @author liuguanghui
+ * @since 2018-12-25
+ */
+@Configuration
+public class SecureConfig extends WebSecurityConfigurerAdapter {
+
+    private final String adminContextPath;
+
+    public SecureConfig(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+
+        http.authorizeRequests()
+                .antMatchers(adminContextPath + "/assets/**").permitAll()
+                .antMatchers(adminContextPath + "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+                .logout().logoutUrl(adminContextPath + "/logout").and()
+                .httpBasic().and()
+                .csrf().disable();
+        // @formatter:on
+    }
+}
