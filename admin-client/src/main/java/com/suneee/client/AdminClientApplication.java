60ffe174541faa7ba5c175fc38c76ee02395e0f3
ah-hui <673350601@qq.com>
2018-12-25 15:51:15 +0800

    [INIT] 初始化仓库

---
+++ admin-client/src/main/java/com/suneee/client/AdminClientApplication.java
@@ -0,0 +1,18 @@
+package com.suneee.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述：.
+ *
+ * @author liuguanghui
+ * @since 2018-12-25
+ */
+@SpringBootApplication
+public class AdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminClientApplication.class, args);
+    }
+}
---
+++ admin-server/src/main/java/com/suneee/server/AdminServerApplication.java
@@ -0,0 +1,22 @@
+package com.suneee.server;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 功能描述：.
+ *
+ * @author liuguanghui
+ * @since 2018-12-25
+ */
+@Configuration
+@SpringBootApplication
+@EnableAdminServer
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+}
